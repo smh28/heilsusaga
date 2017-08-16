01bdc553677112ce7e96a3b8247aa99c06014492
smh28 <sandrahuldudottir@gmail.com>
2017-08-16 10:01:21 +0000

    Initial commit

---
+++ src/pheader.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <!-- Main title, i.e. "Heilsusaga Íslendinga" -->
+        <title><?php echo "Heilsusaga Íslendinga"; ?></title> <!--$Title-->
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <link href="css/main.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div class="pageHeader">
+
+            <div class="banner">
+
+                    <img src="../images/Heilsusaga-isl-01-glaert.png" class="banner-logo" width="372px" height="100px;"/>
+
+                <span>
+                    <img src="../images/if_Flag_of_Iceland_96332.png" class="banner-flags" alt="Icelandic" width="30px" height="30px"/>
+                </span>
+                <span>
+                    <img src="../images/if_Flag_of_United_Kingdom_96354.png" class="banner-flags" alt="English" width="30px" height="30px;"/>
+                </span>
+
+            </div>
+
+
+
+            <nav class="navigation">
+                <ul class="navbar">
+                    <li><a href="login.html"><?php echo "Participate" ?></a>  <!--$Participate-->
+                    <li><a href="about.html"><?php echo "About" ?></a>       <!--$About-->
+                    <li><a href="faq.html"><?php echo "QA" ?></a>            <!--$QA-->
+                    <li><a href="contact.html"><?php echo "Contact" ?></a>   <!--$Contact-->
+                </ul>
+            </nav>
+
+        </div>
+
+
+
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+
+
+    </body>
+
+</html>
