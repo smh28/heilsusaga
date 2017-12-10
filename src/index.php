9978106bbb299f05a1505b1b77b23c738657adcc
smh28 <sandrahuldudottir@gmail.com>
2017-12-10 23:15:14 +0000

    Smá lagfæringar

--- src/index.php
+++ src/index.php
@@ -103,7 +103,7 @@
         <div class="socialMedia">
             <h4>Deildu þessari vefsíðu:</h4>
 
-            <div class="fb-share-button socialMediaItem" data-href="https://heilsusaga.herokuapp.com/" data-layout="button_count" data-size="small" data-mobile-iframe="true">
+            <div class="fb-share-button socialMediaItem" data-href="https://heilsusaga.herokuapp.com/" data-size="small" data-mobile-iframe="true">
                 <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">
                 </a>
             </div>
